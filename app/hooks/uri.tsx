c03a637f9b81f6f8e70c3e39524eba1af2d08386
Yobi <yobibah7295@gmail.com>
2026-03-18 12:59:56 +0000

    fix bugg && add new component

--- app/hooks/uri.tsx
+++ app/hooks/uri.tsx
@@ -1,6 +1,6 @@
 const Uri = () => {
-  const Ai_Token = "AIzaSyArZewIldMZfPUdwugRmM5HMqoMaPY_dWg";
-  const BASE_URL = "AIzaSyBGcBNthdGToZLZgcTZYnAhog-hdRGLmnM";
+  const Ai_Token = "";
+  const BASE_URL = "";
   return { Ai_Token, BASE_URL };
 };
 export default Uri;
